acdd2f6cf3aab9d7efc09456cac39c418972e428
damiforte <109751493+damiforte@users.noreply.github.com>
2025-06-27 18:10:03 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+set -e
 yum update -y
 
 yum install -y epel-release
-yum install -y erlang
-
-# Install RabbitMQ
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.10.0/rabbitmq-server-3.10.0-1.el8.noarch.rpm
-yum localinstall rabbitmq-server-3.10.0-1.el8.noarch.rpm -y
+yum install -y erlang rabbitmq-server
 
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
-
 rabbitmq-plugins enable rabbitmq_management
+
 systemctl stop rabbitmq-server
 echo "XAIFUIBJAVHSEZOKOMHD" > /var/lib/rabbitmq/.erlang.cookie
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
 chmod 400 /var/lib/rabbitmq/.erlang.cookie
 systemctl start rabbitmq-server
 
-# Wait for service to be ready
-sleep 10
+echo "Waiting for RabbitMQ to start..."
+sleep 15
 
-# Create admin user
 export USERNAME="$(aws ssm get-parameter --name /${environment_name}/rabbit/USERNAME --with-decryption --output text --query Parameter.Value --region ${region})"
 export PASS="$(aws ssm get-parameter --name /${environment_name}/rabbit/PASSWORD --with-decryption --output text --query Parameter.Value --region ${region})"
+
+# Create admin user
 rabbitmqctl add_user "$USERNAME" "$PASS"
 rabbitmqctl set_user_tags "$USERNAME" administrator
 rabbitmqctl set_permissions -p / "$USERNAME" ".*" ".*" ".*"
+systemctl restart rabbitmq-server
+
+echo "RabbitMQ installation completed successfully!"
